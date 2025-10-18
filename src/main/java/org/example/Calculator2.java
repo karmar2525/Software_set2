24ecf95073603cb70328ba2d70de7519d863bad4
Karolina Marek <karolina.marek005@gmail.com>
2025-10-18 21:47:04 +0200

    Add division method

--- src/main/java/org/example/Calculator2.java
+++ src/main/java/org/example/Calculator2.java
@@ -11,4 +11,7 @@ public class Calculator2 {
     public int multiply(int a, int b) {
         return a * b;
     }
+    public double divide(int a, int b) {
+        return (double) a / b;
+    }
 }
